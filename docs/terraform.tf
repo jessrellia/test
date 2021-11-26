a68eea3ff4cb209d9f154b03006792a4a9ca67c4
Jessica Aurellia <68703959+jessrellia@users.noreply.github.com>
2021-11-26 11:42:52 +0700

    Hello from GraphQL!

---
+++ docs/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "default-terraform-state-ap-southeast-1-999470832247"
+    dynamodb_table = "default-terraform-state-ap-southeast-1-999470832247"
+    key            = "ap-southeast-1/mfcsis/mfcsis-app/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+  }
+}
